88721171b840a336d420b35c4578b87ca76dd780
devravindersingh <ravindersngh402@outlook.com>
2020-10-08 18:52:29 +0530

    jwt
    without refractor

--- src/main/java/com/ravinder/springsecuritytuts/security/ApplicationSecurityConfig.java
+++ src/main/java/com/ravinder/springsecuritytuts/security/ApplicationSecurityConfig.java
@@ -1,6 +1,8 @@
 package com.ravinder.springsecuritytuts.security;
 
 import com.ravinder.springsecuritytuts.auth.ApplicationUserService;
+import com.ravinder.springsecuritytuts.jwt.JwtTokenVerifier;
+import com.ravinder.springsecuritytuts.jwt.JwtUsernameAndPasswordAuthenticationFilter;
 import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +15,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -44,26 +47,31 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 //                .and()
                 .csrf()
                     .disable()
+                .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager()))
+                .addFilterAfter(new JwtTokenVerifier(),JwtUsernameAndPasswordAuthenticationFilter.class)
                 .authorizeRequests()
                     .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                     .antMatchers("/api/**").hasRole(STUDENT.name())
                     .anyRequest()
-                    .authenticated()
-                .and()
-                .formLogin()
-                    .loginPage("/login")
-                    .permitAll()
-                    .defaultSuccessUrl("/courses",true)
-                .and()
-                .rememberMe()
-                    .userDetailsService(userDetailsServiceBean())
-                .and()
-                .logout()
-                    .logoutUrl("/logout")
-                    .clearAuthentication(true)
-                    .invalidateHttpSession(true)
-                    .deleteCookies("JSESSIONID","remember-me")
-                    .logoutSuccessUrl("/login");
+                    .authenticated();
+//                .and()
+//                .formLogin()
+//                    .loginPage("/login")
+//                    .permitAll()
+//                    .defaultSuccessUrl("/courses",true)
+//                .and()
+//                .rememberMe()
+//                    .userDetailsService(userDetailsServiceBean())
+//                .and()
+//                .logout()
+//                    .logoutUrl("/logout")
+//                    .clearAuthentication(true)
+//                    .invalidateHttpSession(true)
+//                    .deleteCookies("JSESSIONID","remember-me")
+//                    .logoutSuccessUrl("/login");
     }
 
     @Override
